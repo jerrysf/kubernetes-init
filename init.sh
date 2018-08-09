43f322d3e3294cfa11f44f85fdcfc854c386ffc1
root <root@localhost.localdomain>
2018-08-08 22:58:29 -0400

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+systemctl stop firewalld
+systemctl disable firewalld
+
+swapoff -a 
+sed -i 's/.*swap.*/#&/' /etc/fstab
+
+setenforce  0 
+sed -i "s/^SELINUX=enforcing/SELINUX=disabled/g" /etc/sysconfig/selinux 
+sed -i "s/^SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config 
+sed -i "s/^SELINUX=permissive/SELINUX=disabled/g" /etc/sysconfig/selinux 
+sed -i "s/^SELINUX=permissive/SELINUX=disabled/g" /etc/selinux/config  
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl -p /etc/sysctl.d/k8s.conf
+
+#Setup Proxy
+export https_proxy="http://proxy.sin.sap.corp:8080"
+export http_proxy="http://proxy.sin.sap.corp:8080"
+#Excluding Proxy for KUBE API Server
+export no_proxy="10.130.227.241"
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y epel-release
+yum install -y yum-utils device-mapper-persistent-data lvm2 net-tools conntrack-tools wget vim  ntpdate libseccomp libtool-ltdl 
+yum install -y etcd
+
+systemctl enable ntpdate.service
+
+systemctl start ntpdate.service
+ 
+echo "* soft nofile 65536" >> /etc/security/limits.conf
+echo "* hard nofile 65536" >> /etc/security/limits.conf
+echo "* soft nproc 65536"  >> /etc/security/limits.conf
+echo "* hard nproc 65536"  >> /etc/security/limits.conf
+echo "* soft  memlock  unlimited"  >> /etc/security/limits.conf
+echo "* hard memlock  unlimited"  >> /etc/security/limits.conf
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
